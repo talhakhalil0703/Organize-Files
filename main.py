ce2697ddbb3d7ee761ef762aa3ee70ace2522a0b
talhakhalil0703 <tkhalil0703@gmail.com>
2019-08-27 10:53:24 -0600

    Added auto sorting

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def find_file_directory(data_path, stringendswith):
         for file in files:
             if file.endswith(stringendswith):
                 file_directory.append(os.path.join(root, file))
-
+        break
     file_directory.sort()
     return file_directory
 
@@ -28,7 +28,6 @@ def find_substring_all_occurences(stringtocheck, substring):
 def movetofolder(datapath, substring, foldername):
     allfiles = find_file_directory(datapath, substring)
     for file in allfiles:
-
         newdestinationindex = max(find_substring_all_occurences(allfiles[0], '/'))
         if not os.path.exists(file[0:newdestinationindex] + '/' + foldername):
             os.makedirs(file[0:newdestinationindex] + '/' + foldername)
@@ -54,6 +53,22 @@ def browse_clicked():
     what_pathname.delete(0, END)
     what_pathname.insert(END, window.filename)
 
+def auto_sort_clicked():
+    pathname = what_pathname.get()
+    if pathname == '':
+        print('Please choose a directory')
+        return
+    movetofolder(pathname, '.PNG', 'Images')
+    movetofolder(pathname, '.csv', 'Excel')
+    movetofolder(pathname, '.xlsx', 'Excel')
+    movetofolder(pathname, '.docx', 'Word')
+    movetofolder(pathname, '.doc', 'Word')
+    movetofolder(pathname, '.pptx', 'Powerpoint')
+    movetofolder(pathname, '.dmg', 'Software Installer')
+    movetofolder(pathname, '.pdf', 'PDF')
+    movetofolder(pathname, '.mov', 'Videos')
+    print('Done')
+
 what_pathname_label = Label(window, text = 'What is the pathname: ')
 what_file_extensions_label = Label(window, text = 'What do the file extensions end with: ')
 what_folder_move_to_label = Label(window, text = 'What is the name of the folder you want them to: ')
@@ -68,9 +83,11 @@ what_folder_move_to.grid(column = 1, row = 2)
 move_files_button = Button(window, text = 'Move Files', command = movefiles)
 exit_button = Button(window, text = 'Exit', command = exit_program)
 browse_button = Button(window, text = 'Browse', command = browse_clicked)
+auto_sort_button = Button(window, text = 'Auto Sort', command = auto_sort_clicked)
 
 move_files_button.grid(column = 0, row = 3)
-exit_button.grid(column = 1, row = 3)
+exit_button.grid(column = 2, row = 3, padx = (20,20))
 browse_button.grid(column = 2, row = 0, padx = (20,20))
+auto_sort_button.grid(column = 1, row = 3)
 
 window.mainloop()
